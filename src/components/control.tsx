99c8c1cc3193652ed58281882886c2016c6ddf52
wenfeng xing <wenfeng.xing@weareadaptive.com>
2022-06-15 17:12:31 -0400

    add the state for control component

--- src/components/control.tsx
+++ src/components/control.tsx
@@ -1,8 +1,29 @@
+import { useState } from "react"
+
 export default function Control() {
+    const [lapResetButton, setLapResetButton] = useState("Lap");
+    const [startStopButton, setStartStopButton] = useState("Start");
+
+    const handleLapResetButton = () => {
+        if (lapResetButton === "Lap") {
+            setLapResetButton("Reset");
+        } else if (lapResetButton === "Reset") {
+            setLapResetButton("Lap");
+        }
+    }
+
+    const handleStartStopButton = () => {
+        if (startStopButton === "Start") {
+            setStartStopButton("Stop");
+        } else if (startStopButton === "Stop") {
+            setStartStopButton("Start");
+        }
+    }
+
     return (
         <section className="wrapper-control">
-            <button>Lap</button>
-            <button>Start</button>
+            <button onClick={handleLapResetButton}>{lapResetButton}</button>
+            <button onClick={handleStartStopButton}>{startStopButton}</button>
         </section>
     )
 }
